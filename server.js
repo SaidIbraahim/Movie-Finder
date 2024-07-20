69d12c8269df2d7457c33681d9a92db925eb2413
Said Ibrahim <145333786+SaidIbraahim@users.noreply.github.com>
2024-07-21 00:01:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY; 
 
 app.set('view engine', 'ejs');
